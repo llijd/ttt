a7475f3bfe5eead1bb0376ea3581f7fb36032957
llijd <m17377797887@163.com>
2025-11-28 14:57:57 +0800

    Rename output variables in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "this vpc id" {
+output "this_vpc_id" {
   value = alicloud_vpc.default.id
 }
-output "this swicth name" {
+output "this_swicth_name" {
   value = var.vs_name
 }
-output "this vpc cidr_block" {
+output "this_vpc_cidr_block" {
   value = var.vpc_cidr_block
 }
